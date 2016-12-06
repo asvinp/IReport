81e1e554f233ae07977d9722421c5c0168a621fd
AISHWARYA PATTWARDHAN <aishwarya.patwardhan123@gmail.com>
2016-12-06 01:21:53 -0800

    resident profile changes

--- app/src/main/java/com/cmpe277group4/ireport/ProfileActivity.java
+++ app/src/main/java/com/cmpe277group4/ireport/ProfileActivity.java
@@ -2,12 +2,25 @@ package com.cmpe277group4.ireport;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    TextView name, address, email;
+    Button signout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        address = (TextView) findViewById(R.id.Address);
+        name = (TextView) findViewById(R.id.Name);
+        email = (TextView) findViewById(R.id.Email);
+
+
+        email.setText(SaveSharedPreference.getUserId(ProfileActivity.this));
+
     }
 }
