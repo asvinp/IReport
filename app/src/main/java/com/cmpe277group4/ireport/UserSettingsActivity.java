1059ac4a53f606692102182bca1227169626c3ce
AISHWARYA PATTWARDHAN <aishwarya.patwardhan123@gmail.com>
2016-12-08 15:43:47 -0800

    n

--- app/src/main/java/com/cmpe277group4/ireport/UserSettingsActivity.java
+++ app/src/main/java/com/cmpe277group4/ireport/UserSettingsActivity.java
@@ -1,14 +1,41 @@
 package com.cmpe277group4.ireport;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.entity.StringEntity;
 
 public class UserSettingsActivity extends AppCompatActivity {
     private Switch emailNotification , statusChange , anonymous;
     //switchEmailNotification switchStatusChange switchAnonymous
+
+    private AsyncHttpClient updateClient = new AsyncHttpClient();
+    private JSONObject serverDataJSON = new JSONObject();
+    private StringEntity serverDataEntity;
+
+    private static final String PROFILE_TAG = "PROFILE";
+    private Button saveButtonText;
+    private int eNot = 1;
+    private int sChange = 1;
+    private int  ana = 0;
+    String email;
+    public String resident_id = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +45,8 @@ public class UserSettingsActivity extends AppCompatActivity {
 
         statusChange = (Switch) findViewById(R.id.switchStatusChange);
         anonymous = (Switch) findViewById(R.id.switchAnonymous);
+        Intent intent = getIntent();
+        resident_id = intent.getExtras().getString("resident_id");
 
 
         //set the switch to ON
@@ -32,11 +61,14 @@ public class UserSettingsActivity extends AppCompatActivity {
                 if(isChecked){
                     //switchStatus.setText("Switch is currently ON");
                     anonymous.setChecked(false);
+                    eNot = 1;
                     Log.d("notification ","is on ");
                 }else{
                     //switchStatus.setText("Switch is currently OFF");
 
                     Log.d("notification ","is off ");
+                    eNot = 0;
+
                 }
 
             }
@@ -53,10 +85,12 @@ public class UserSettingsActivity extends AppCompatActivity {
                     anonymous.setChecked(false);
 
                     Log.d("notification ","is on ");
+                    sChange = 1;
                 }else{
                     //switchStatus.setText("Switch is currently OFF");
 
                     Log.d("notification ","is off ");
+                    sChange = 0 ;
                 }
 
             }
@@ -74,11 +108,14 @@ public class UserSettingsActivity extends AppCompatActivity {
                     emailNotification.setChecked(false);
 
                     Log.d("notification ","is on ");
+                    ana =1 ;
+
 
                 }else{
                     //switchStatus.setText("Switch is currently OFF");
 
                     Log.d("notification ","is off ");
+                    ana = 0 ;
                 }
 
             }
@@ -95,5 +132,45 @@ public class UserSettingsActivity extends AppCompatActivity {
 
             Log.d("notification ","is off 2 ");
         }
+
+//eNot sChange ana
+
+        saveButtonText = (Button) findViewById(R.id.buttonSave);
+        saveButtonText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                try {
+                    //emailNotification , statusChange , anonymous;
+
+                    serverDataJSON.put("resident_id",resident_id);
+                    serverDataJSON.put("emailNotification",eNot);
+                    serverDataJSON.put("statusChange",sChange);
+                    serverDataJSON.put("anonymous",ana);
+                    serverDataEntity = new StringEntity(serverDataJSON.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+
+                updateClient.get(UserSettingsActivity.this, getString(R.string.server_url) + "updateSettings", serverDataEntity, "application/json", new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        Log.d(PROFILE_TAG,"User profile Updated");
+                        Intent reportActivity = new Intent(UserSettingsActivity.this,ReportActivity.class);
+                        startActivity(reportActivity);
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                        Log.d(PROFILE_TAG,  " Failure Status : " + Integer.toString(statusCode));
+                    }
+                });
+
+            }
+        });
+
     }
-    }
+
+}
