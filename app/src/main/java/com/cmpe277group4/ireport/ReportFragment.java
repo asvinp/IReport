4da945421b589a71d41591fcaa0b4e744bc9749a
Vinay <vinayshiremath@gmail.com>
2016-12-07 19:32:41 -0800

    Storing pics with custom name

--- app/src/main/java/com/cmpe277group4/ireport/ReportFragment.java
+++ app/src/main/java/com/cmpe277group4/ireport/ReportFragment.java
@@ -8,13 +8,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +29,11 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -39,8 +48,18 @@ public class ReportFragment extends Fragment {
     private Spinner mSeveritySpinner;
     private ImageButton fetchuserloc;
     private TextView mLatLng;
+    private Button mSendButton;
     private static double ulatitude=0;
     private static double ulongitude=0;
+    private String TAG = "ReportFraagment";
+    private String FILE_NAME;
+
+    private Bitmap mImageBitmap;
+    private static final String CAMERA_DIR = "/dcim/";
+    private static final String JPEG_FILE_PREFIX = "IMG_";
+    private static final String JPEG_FILE_SUFFIX = ".jpg";
+    private static final String FILE_PATH = Environment.getExternalStorageDirectory()+CAMERA_DIR;
+    private String mCurrentPhotoPath;
 
     private static final String[] INITIAL_PERMS={
             android.Manifest.permission.ACCESS_FINE_LOCATION
@@ -54,17 +73,140 @@ public class ReportFragment extends Fragment {
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Log.d(TAG, "dispatchTakePictureIntent outside if statement");
         if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            Log.d(TAG, "dispatchTakePictureIntent within if statement");
+            File f = null;
+            try {
+                f = setUpPhotoFile();
+                mCurrentPhotoPath = f.getAbsolutePath();
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+            } catch (IOException e) {
+                e.printStackTrace();
+                f = null;
+                mCurrentPhotoPath = null;
+            }
+
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
+    }
+
+    private String getAlbumName() {
+        return getString(R.string.album_name);
+    }
+
+    public File getAlbumStorageDir(String albumName) {
+        return new File(
+                Environment.getExternalStorageDirectory()
+                        + CAMERA_DIR
+                        + albumName
+        );
+    }
+
+    private File getAlbumDir() {
+        File storageDir = null;
+
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+
+            storageDir = getAlbumStorageDir(getAlbumName());
+
+            if (storageDir != null) {
+                if (! storageDir.mkdirs()) {
+                    if (! storageDir.exists()){
+                        Log.d("CameraSample", "failed to create directory");
+                        return null;
+                    }
+                }
+            }
+
+        } else {
+            Log.v(getString(R.string.app_name), "External storage is not mounted READ/WRITE.");
+        }
+
+        return storageDir;
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String filename = JPEG_FILE_PREFIX + timeStamp + "_";
+        File albumF = getAlbumDir();
+        File imageF = File.createTempFile(filename, JPEG_FILE_SUFFIX, albumF);
+        Log.d(TAG,"Printing absolute path of file stored"+imageF.getAbsolutePath());
+        return imageF;
+    }
+
+    private File setUpPhotoFile() throws IOException {
+
+        File f = createImageFile();
+        mCurrentPhotoPath = f.getAbsolutePath();
+        Log.d(TAG, mCurrentPhotoPath);
+
+        return f;
+    }
+
+    private void setPic() {
+
+		/* There isn't enough memory to open up more than a couple camera photos */
+		/* So pre-scale the target bitmap into which the file is decoded */
+
+        Log.d(TAG,"setPic() called");
+		/* Get the size of the ImageView */
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+		/* Get the size of the image */
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+		/* Figure out which way needs to be reduced less */
+        int scaleFactor = 1;
+        if ((targetW > 0) || (targetH > 0)) {
+            scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+        }
+
+		/* Set bitmap options to scale the image decode target */
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+		/* Decode the JPEG file into a Bitmap */
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+
+		/* Associate the Bitmap to the ImageView */
+        mImageView.setImageBitmap(bitmap);
+        mImageView.setVisibility(View.VISIBLE);
+    }
+
+    private void galleryAddPic() {
+        Log.d(TAG,"galleryAddPic() called");
+        Intent mediaScanIntent = new Intent("android.intent.action.MEDIA_SCANNER_SCAN_FILE");
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        getActivity().sendBroadcast(mediaScanIntent);
+    }
+
+    private void handleCameraPhoto(Intent intent) {
+
+        if (mCurrentPhotoPath != null) {
+            setPic();
+            galleryAddPic();
+//            Bundle extras = intent.getExtras();
+//            mImageBitmap = (Bitmap) extras.get("data");
+//            mImageView.setImageBitmap(mImageBitmap);
+            mCurrentPhotoPath = null;
+        }
+
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            mImageView.setImageBitmap(imageBitmap);
+            handleCameraPhoto(data);
         }
     }
 
@@ -86,6 +228,14 @@ public class ReportFragment extends Fragment {
         mSizeSpinner = (Spinner)v.findViewById(R.id.spinnerSize);
         mSeveritySpinner = (Spinner)v.findViewById(R.id.spinnerSeverity);
         mLatLng = (TextView)v.findViewById(R.id.textViewLatLng);
+        mSendButton = (Button)v.findViewById(R.id.sendButton);
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, FILE_NAME);
+            }
+        });
 
         if (!canAccessLocation()) {
             requestPermissions(INITIAL_PERMS, INITIAL_REQUEST);
